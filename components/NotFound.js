cbec8ffd82da8823b7bbd76ad5950ea2265c032d
Tiago rosa da costa <tiagorosadacost@gmail.com>
2022-01-19 23:31:03 -0300

    Delete unnecessary code

--- components/NotFound.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default ({ isNotFound }) => {
-    if (!isNotFound) return false;
-
-    return (
-        <>Event don't available!!!!</>
-    )
-}
--- components/Preloader.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export default ({ isShow }) => {
-    if (!isShow) return false;
-    return <>Loading....</>
-}
